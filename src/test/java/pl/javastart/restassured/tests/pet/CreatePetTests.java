65d300ad261119e5968cc41ce50a5fb40ecfe1cb
Luk <lukasz.bola@icloud.com>
2021-07-07 13:11:16 +0200

    implementacja mechanizmu sprzątania po testach w @AfterMethod

--- src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
+++ src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
@@ -1,6 +1,8 @@
 package pl.javastart.restassured.tests.pet;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+import pl.javastart.restassured.main.pojo.ApiResponse;
 import pl.javastart.restassured.main.pojo.pet.Pet;
 import pl.javastart.restassured.main.test.data.PetDataGenerator;
 import pl.javastart.restassured.tests.testbases.SuiteTestBase;
@@ -10,19 +12,32 @@ import static org.testng.Assert.assertEquals;
 
 public class CreatePetTests extends SuiteTestBase {
 
+    private Pet pet;
+
     @Test
     public void givenPetWhenPostPetThenPetIsCreatedTest() {
 
         PetDataGenerator petDataGenerator = new PetDataGenerator();
-        Pet pet = petDataGenerator.generatePet();
+        pet = petDataGenerator.generatePet();
 
-        Pet actualPet = given().log().all().body(pet).contentType("application/json")
+        Pet actualPet = given().body(pet).contentType("application/json")
                 .when().post("pet")
-                .then().statusCode(200).log().all().extract().as(Pet.class);
+                .then().statusCode(200).extract().as(Pet.class);
 
 
         assertEquals(actualPet.getId(), pet.getId(), "Pet id");
         assertEquals(actualPet.getName(), pet.getName(), "Pet name");
-
     }
+
+    @AfterMethod
+    public void cleanUpAfterTest() {
+        ApiResponse apiResponse = given().contentType("application/json")
+                .when().delete("pet/{petId}", pet.getId())
+                .then().statusCode(200).extract().as(ApiResponse.class);
+
+        assertEquals(apiResponse.getCode(), Integer.valueOf(200), "Code");
+        assertEquals(apiResponse.getType(), "unknown", "Type");
+        assertEquals(apiResponse.getMessage(), pet.getId().toString(), "Type");
+    }
+
 }
